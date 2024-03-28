edf9f88640b5b4e01965a3e300f2b7ffbfa3b77b
JLi2007 <LiJames9532@gmail.com>
2024-03-28 12:59:35 -0400

    Updates - Mar 27/28

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,62 @@
 import '../style/style.css';
 
 function Home() {
+
   return (
     <div className='background-container'>
-      <h1>Welcome to My Portfolio</h1>
-      <p>This is the home page.</p>
-      <img src="../../assets/logo.png" style={{width:'400px', height:'400px'}}></img>
+      <div className="container text-center">
+        <div className="row">
+          <div id="col1" className="col">
+          <div id="carouselExampleCaptions" className="carousel slide">
+              <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img src="../../assets/carousel1.jpg" className="d-block w-100" alt="..."/>
+                  <div className="carousel-caption d-none d-md-block">
+                    <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                  </div>
+                </div>
+                <div className="carousel-item">
+                  <img src="../../assets/carousel2.jpg" className="d-block w-100" alt="..."/>
+                  <div className="carousel-caption d-none d-md-block">
+                    <h5>Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                  </div>
+                </div>
+                <div className="carousel-item">
+                  <img src="../../assets/carousel3.jpg" className="d-block w-100" alt="..."/>
+                  <div className="carousel-caption d-none d-md-block">
+                    <h5>Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                  </div>
+                </div>
+              </div>
+              <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+              </button>
+              <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+              </button>
+            </div>
+
+          </div>
+
+          <div id="col2" className="col">
+            <h1>Welcome to My Portfolio</h1>
+            <p>This is the home page.</p>
+            <img src="../../assets/logo.png" style={{width:'400px', height:'400px'}}></img>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,29 +5,29 @@ function Navbar(){
     return(
         //Add a bootstrap overrides ID
         <nav id="navbar-overides" className="navbar navbar-expand-lg bg-body-tertiary">
-        <div id="bootstrap-overides" className="container-fluid">
-            <img src="../../assets/logo.png" style={{width: '25px', height: '25px'}}></img>
-            <a className="navbar-brand">Porfolio</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                <Link to="/" id="active-navbar" className="nav-link" aria-current="page">Home</Link>
-                </li>
-                <li className="nav-item">
-                <Link to="/experience" className="nav-link" aria-current="page">Experience</Link>
-                </li>
-                <li className="nav-item">
-                <Link to="/projects" className="nav-link" aria-current="page">Projects</Link>
-                </li>
-                <li className="nav-item">
-                <Link to="/quotes" className="nav-link" aria-current="page">Quotes</Link>
-                </li>
-            </ul>
+            <div id="bootstrap-overides" className="container-fluid">
+                <img src="../../assets/logo.png" style={{width: '25px', height: '25px'}}></img>
+                <a className="navbar-brand">Porfolio</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                    <Link to="/" id="active-navbar" className="nav-link" aria-current="page">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link to="/experience" className="nav-link" aria-current="page">Experience</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link to="/projects" className="nav-link" aria-current="page">Projects</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link to="/quotes" className="nav-link" aria-current="page">Quotes</Link>
+                    </li>
+                </ul>
+                </div>
             </div>
-        </div>
         </nav>
     )
 }
