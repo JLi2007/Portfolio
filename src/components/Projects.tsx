33c272abfe57c8195a08008283a249c4fdf441c6
JLi2007 <LiJames9532@gmail.com>
2024-04-03 22:03:05 -0400

    Projects section - April 2-3

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,24 +5,30 @@ function Projects() {
     <>
     <div id="bootstrap-overides-projects" className='background-container'>
       <p>This is the projects page.</p>
-      <div id="boxes" className="container text-center">
-        <div id="box" className="row justify-content-between">
-          <div className="col-4">
+      <div id="boxes-group" className="container text-center">
+        <div id="boxes" className="row justify-content-center">
+          <div className="col-4 box">
+            One of column
+          </div>
+        </div>
+
+        <div id="boxes" className="row justify-content-between">
+          <div className="col-4 box">
             One of two columns
           </div>
-          <div className="col-4">
+          <div className="col-4 box">
             One of two columns
           </div>
         </div>
-        
-        <div id="box" className="row justify-content-evenly">
-          <div className="col-3">
+
+        <div id="boxes" className="row justify-content-evenly">
+          <div className="col-3 box">
             One of three columns
           </div>
-          <div className="col-3">
+          <div className="col-3 box">
             One of three columns
           </div>
-          <div className="col-3">
+          <div className="col-3 box">
             One of three columns
           </div>
         </div>
