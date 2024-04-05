43451be9e01209455956dabab3d1fda393695e75
JLi2007 <LiJames9532@gmail.com>
2024-04-05 14:25:36 -0400

    Quotes + typing effect

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,12 +1,30 @@
 import '../style/style.css';
+import Typewriter from 'typewriter-effect';
 
 function Quote() {
   return (
-    <>
-    <div className='background-container'>
+    <div id="bootstrap-overides-quotes" className='background-container'>
+
+      <div className="cinema-img">
+
+      </div>
+
       <p>This is the quotes page. Idea-movie and typewriter effect</p>
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter.typeString('Hello World!')
+            .callFunction(() => {
+              console.log('String typed out!');
+            })
+            .pauseFor(2500)
+            .deleteAll()
+            .callFunction(() => {
+              console.log('All strings were deleted');
+            })
+            .start();
+        }}
+      />
     </div>
-    </>
   );
 }
 
---
+++ src/components/quotes.ts
@@ -0,0 +1,16 @@
+interface Quote{
+    text: string;
+    author: string;
+}
+
+export const quotes: Quote[]=[
+    {text: "Dreams are not what you see in your sleep, dreams are things which do not let you sleep.", author:"Cristiano Ronaldo"},
+    {text: "Knowing is not enough, we must apply. Willing is not enough, we must do", author:"Bruce Lee"},
+    {text: "Don't think small and expect to have a big life.", author:"Marianne Williamson"},
+    {text: "If opportunity doesn't knock, build a door", author:"Milton Berle"},
+    {text: "I shut my eyes in order to see.", author:"Paul Gauguin"},
+    {text:"Nobody has enough talent to live on talent alone. Even when you have talent, a life without work goes nowhere.", author:"Arsene Wenger"},
+    {text:"Hard work beats talent when talent doesn't work hard", author:"Tim Notke"},
+    {text:"Talent hits a target no one else can hit; Genius hits a target no one else can see", author:"Arthur Schopenhauer"},
+    {text:"😂Be happy!😃", author:"Me"}
+]
