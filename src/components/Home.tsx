5996da641413f63647c47e27b21a51b4cd60b358
JLi2007 <LiJames9532@gmail.com>
2024-04-26 19:06:01 -0400

    Home page styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -60,29 +60,30 @@ function Home() {
 
             <div id="inside">
               <h3 className="gradient" style={{padding: '5px'}}>Meet James</h3>
-              <p>Coming from Waterloo, <span style={{color:'#ef4d4d'}}>James Li</span> is a high school student with aspirations in Computer Science. 
-                Beginning to <span><a style={{color: '#65e393'}} href="https://github.com/JLi2007">program</a></span> only as recently as May 2023, he is unfazed by the late onset. He sees it only as fuel.
+              <p>Coming from Waterloo, <span className="gradient1">James Li</span> is a high school student with aspirations in Computer Science. 
+                Beginning to <span><a style={{color: '#65e393'}} href="https://github.com/JLi2007">program</a></span> only as recently as <strong>May 2023</strong>, he is unfazed by the late onset. He sees it only as fuel.
                 Since then, James has attended <span><a href="https://devpost.com/lijames9532" style={{color: '#a567ec'}}>3 Hackathons</a></span> and obtained a executive position at <span><a href="https://rythmhacks.ca/" style={{color:'#6583e8'}}>Rythmhacks</a></span>, 
-                a hackathon that aims to showcase youth innovation. 
+                a hackathon that aims to showcase <span className='gradient2'>youth innovation</span>. 
               </p>
 
-              <p>Having delved into coding later than many, he constantly reminds himself how life isn't a race. 
-                Striving to focus on self-improvement, James is deeply devoted to becoming <span className='gradient1'>1% better than yesterday</span>.
+              <p>Having delved into coding later than many, he constantly reminds himself how <strong>life isn't a race. </strong> 
+                Striving to focus on self-improvement, James is deeply devoted to becoming <span className='gradient3'>1% better than yesterday</span>.
               </p>
 
               <p>One project that's keeping him busy in 2024 is his <span><a href="https://github.com/JLi2007/TS_WeatherProject_Mongo" style={{color:'#4defe4'}}>weather project</a></span>. 
-                Referred by him as a “journey” that showcases his growth as a developer. 
+                Referred by him as a <span className='gradient4'>journey</span> that showcases his growth as a developer. 
               </p>
 
               <p style={{paddingBottom: '20px'}}>It's not all about academics.
-                Football⚽ has been one of his greatest passions since young. 
-                Described as a disciplined player with high game IQ, he trains with an <span><a href="https://www.bvbinternationalacademy-waterloo.com/" style={{color:'#f5e16f'}}>academy</a></span>. 
-                He is also a part of a competitive school team that has vivid hopes to make it far into the high school bracket.
+                <span className='gradient5'> Football</span>⚽ has been one of his greatest <strong>passions</strong> since young. 
+                Described as a disciplined player with high game IQ, he trains with an <span><a href="https://www.bvbinternationalacademy-waterloo.com/" style={{color:'#e9d45c'}}>academy</a></span>. 
+                He is also a part of a <strong>competitive</strong> school team that has vivid hopes to make it far into the high school bracket.
               </p>
             </div>
 
             <div className="button">
-              <button type="button" id="button" className="btn btn-outline-info"><a href="https://drive.google.com/file/d/1s73bKW2nadhLyfqTex0mMzrZZyXRfGAQ/view?usp=sharing" style={{textDecoration:'none', fontWeight:'bold'}}>View my Resume</a></button>
+              <button type="button" id="resume-button-resume" className="btn btn-outline-info"><a href="https://drive.google.com/file/d/1s73bKW2nadhLyfqTex0mMzrZZyXRfGAQ/view?usp=sharing" style={{textDecoration:'none', fontWeight:'600'}}>View Resume</a></button>
+              <button type="button" id="resume-button-linkedin" className="btn btn-outline-info"><a href="https://www.linkedin.com/in/james-li-a81004275/" style={{textDecoration:'none', fontWeight:'600'}}>View Linkedin</a></button>
             </div>
 
           </div>
